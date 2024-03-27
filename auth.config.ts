aea9d7ed34f40d05124bbf9e8560af798eaf8bd0
cuongph <cuongph@runsystem.net>
2024-03-27 11:45:28 +0700

    fix: update logic checkauth

--- auth.config.ts
+++ auth.config.ts
@@ -3,20 +3,15 @@ import { JWT } from "next-auth/jwt";
 
 export const authConfig = {
   pages: {
-    signIn: "/",
+    signIn: "/home",
     signOut: "/home",
     error: "/",
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
-      // const isLoggedIn = !!auth?.user;
-      // const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      // if (isOnDashboard) {
-      //   if (isLoggedIn) return true;
-      //   return false; // Redirect unauthenticated users to login page
-      // } else if (isLoggedIn) {
-      //   return Response.redirect(new URL('/dashboard', nextUrl));
-      // }
+      const isLoggedIn = !!auth?.user;
+      const isOnHome = nextUrl.pathname.startsWith("/home");
+      if (!isLoggedIn) return false;
       return true;
     },
     async session({
--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,11 @@
-import type { NextRequest } from "next/server";
-
-export function middleware(request: NextRequest) {
-  const session = request.cookies.get("authjs.session-token")?.value;
-  if (!session && !request.nextUrl.pathname.startsWith("/home")) {
-    return Response.redirect(new URL("/home", request.url));
-  }
-  if (session && request.nextUrl.pathname === "/") {
-    return Response.redirect(new URL("/home", request.url));
-  }
-}
+export { auth as middleware } from "./auth";
 
 export const config = {
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
      * - api (API routes)
-     * - _next/static (static files)
+     * - _next/static (static files)hah
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
