f07ab1f5fcd298aa6f5661f2d7c385e5309d2f15
cuongph <cuongph@runsystem.net>
2024-03-29 00:43:56 +0700

    fix: #129 update code

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { Providers } from "./providers";
 import ToastLayout from "@/components/atoms/Toast";
 import ReduxProvider from "@/provider/redux/redux.provider";
 import NextAuthProvider from "@/provider/next-auth/next-auth.provider";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,7 @@ const theme: DefaultTheme = {
 };
 export const metadata: Metadata = {
   title: "Foodly | UIT Team",
-  description: "App Oder food and drink",
+  description: "App Order food and drink",
   viewport:
     "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
 };
@@ -32,6 +33,27 @@ export default function RootLayout({
   return (
     <ReduxProvider>
       <html lang="en" suppressHydrationWarning>
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <meta name="theme-color" content="#ffffff" />
+        </Head>
         <body
           className={clsx(
             inter.className,
--- components/molecules/Header/index.tsx
+++ components/molecules/Header/index.tsx
@@ -2,26 +2,52 @@
 import { useAppSelector } from "@/hooks/stores.hook";
 import { Avatar } from "@nextui-org/react";
 import { useSession } from "next-auth/react";
+import Image from "next/image";
 import styled from "styled-components";
-
+import logo from "@/public/images/logo.webp";
+import { useWindowSize } from "@/hooks/window-size";
+import clsx from "clsx";
 const HeaderWrapper = styled.div``;
 
 const AvatarWrapper = styled(Avatar)``;
 
 export default function Header() {
   const session = useSession();
-  return session.status === "authenticated" ? (
-    <HeaderWrapper className="absolute top-[10px] right-[10px] cursor-pointer flex flex-row items-center gap-3">
-      <h3 className="m-0 text-white font-bold text-[14px]">
-        {session.data.user.authenticated_data?.email}
-      </h3>
-      <AvatarWrapper
-        isBordered
-        color="primary"
-        src="https://i.pravatar.cc/150?u=a04258a2462d826712d"
-      />
+  const { isMobile } = useWindowSize();
+
+  return (
+    <HeaderWrapper
+      className={clsx(
+        "absolute top-[10px] right-[0px] flex flex-row w-full pl-4 pr-4",
+        isMobile ? "justify-center" : "justify-between"
+      )}
+    >
+      {/* bg-[#ffffff14] */}
+      <div className="flex flex-row items-center gap-2 backdrop:blur-md px-2 py-1 backdrop-blur-md bg-[#fe724c91] rounded-[30px]">
+        <Image
+          width={40}
+          height={40}
+          src={logo.src}
+          alt="foodly booking"
+          className="rounded-[30px]"
+        />
+        <span className="font-bold text-[18px] text-white">FOODLY BOOKING</span>
+      </div>
+      {session.status === "authenticated" && !isMobile && (
+        <div className="flex flex-row  items-center gap-3 ">
+          {!isMobile && (
+            <h3 className="m-0 text-white font-bold text-[14px]">
+              {session.data.user.authenticated_data?.email}
+            </h3>
+          )}
+
+          <AvatarWrapper
+            isBordered
+            color="primary"
+            src="https://i.pravatar.cc/150?u=a04258a2462d826712d"
+          />
+        </div>
+      )}
     </HeaderWrapper>
-  ) : (
-    <></>
   );
 }
